641327b788213efa3aec2d5b65db10b6eb7ec3ec
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-01 21:46:34 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -2,15 +2,14 @@ package classroom;
 
 public enum Tipo {
 	DISCIPLINAR("DISCIPLINAR"),FUNDAMENTACION("FUNDAMENTACION"),ELECTIVA("ELECTIVA");
-	
+
     private String nombre;
     
     private Tipo(String nombre) {
     	this.nombre=nombre;
     }
-    
+
     public String getNombre() {
     	return nombre;
     }
-    
-	}
+}
